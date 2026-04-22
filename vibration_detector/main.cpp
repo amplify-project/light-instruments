56c86c7b4b85fe90854c3415cca093a45dfb65d9
Thomas Röggla <t.roggla@cwi.nl>
2026-04-22 15:01:10 +0200

    adjusted threshold value for percussion instrument

--- vibration_detector/main.cpp
+++ vibration_detector/main.cpp
@@ -3,13 +3,25 @@
 #include <ArduinoJson.h>
 
 uint8_t broadcastAddress[] = {0x2C, 0xF4, 0x32, 0x4E, 0xB2, 0xBE};
-String deviceName = "";
+String deviceName = "percussion_big";
 
 const int analogPin = A1;
-const int threshold = 15; // Ignore minor voltage jitter
+const int threshold = 5; // Ignore minor voltage jitter
 
 // State tracking
 int lastValue = -1;
+
+void sendJsonData(int val) {
+  StaticJsonDocument<128> doc;
+  doc["device"] = deviceName;
+  doc["val"] = val;
+
+  char buffer[128];
+  serializeJson(doc, buffer);
+  esp_now_send(broadcastAddress, (uint8_t *)buffer, strlen(buffer) + 1);
+
+  Serial.printf("Value: %d\n", val);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -48,16 +60,4 @@ void loop() {
   }
 
   delay(20);
-}
-
-void sendJsonData(int val) {
-  StaticJsonDocument<128> doc;
-  doc["device"] = deviceName;
-  doc["val"] = val;
-
-  char buffer[128];
-  serializeJson(doc, buffer);
-  esp_now_send(broadcastAddress, (uint8_t *)buffer, strlen(buffer) + 1);
-
-  Serial.printf("Value: %d\n", val);
 }
