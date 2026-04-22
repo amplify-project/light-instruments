2016e39ee9a1512f3a14f929f390fad84d23dec9
Thomas Röggla <t.roggla@cwi.nl>
2026-04-22 14:48:57 +0200

    added calibration minima and value remapping for touch instrument

--- touch_instrument/main.cpp
+++ touch_instrument/main.cpp
@@ -5,11 +5,37 @@
 uint8_t broadcastAddress[] = {0x2C, 0xF4, 0x32, 0x4E, 0xB2, 0xBE};
 String deviceName = "touch";
 const int touchPins[] = {D1, D2, D3};
+const int touchMinima[] = {39000, 44000, 45000};
 const int numInputs = 3;
 const int sensitivityThreshold = 3; // Minimum change to trigger a send
 
 // State tracking
 int lastValues[] = {0, 0, 0};
+
+void sendJsonData(int id, int val) {
+  int adjustedVal = val - touchMinima[id - 1];
+
+  if (adjustedVal < 0) {
+    adjustedVal = 0;
+  }
+
+  if (adjustedVal > 50000) {
+    adjustedVal = 50000;
+  }
+
+  adjustedVal = map(adjustedVal, 0, 50000, 0, 4096);
+
+  JsonDocument doc;
+  doc["device"] = deviceName;
+  doc["id"] = id;
+  doc["val"] = adjustedVal;
+
+  char buffer[128];
+  serializeJson(doc, buffer);
+
+  esp_now_send(broadcastAddress, (uint8_t *) buffer, strlen(buffer) + 1);
+  Serial.printf("ID: %d | Raw Value: %d | Adjusted Value: %d\n", id, val, adjustedVal);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -46,18 +72,4 @@ void loop() {
   }
 
   delay(30);
-}
-
-void sendJsonData(int id, int val) {
-  StaticJsonDocument<128> doc;
-  doc["device"] = deviceName;
-  doc["id"] = id;
-  doc["val"] = val;
-
-  char buffer[128];
-  serializeJson(doc, buffer);
-
-  esp_now_send(broadcastAddress, (uint8_t *) buffer, strlen(buffer) + 1);
-
-  Serial.printf("ID: %d | Raw Value: %d\n", id, val);
 }
