f872efef39525c88f13411cfcc67741fedb9c065
Thomas Röggla <t.roggla@cwi.nl>
2026-07-18 16:45:31 +0200

    added class for implementing custom light commands

---
+++ controller/Globals.h
@@ -0,0 +1,11 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <FastLED.h>
+
+#define NUM_LEDS_D0 30
+#define DATA_PIN_D0 D0
+
+extern CRGB ledsD0[NUM_LEDS_D0];
+
+#endif // GLOBALS_H
---
+++ controller/LightCommand.h
@@ -0,0 +1,19 @@
+#ifndef LIGHT_COMMAND_H
+#define LIGHT_COMMAND_H
+
+#include <ArduinoJson.h>
+
+class LightCommand {
+public:
+    virtual ~LightCommand() = default;
+    virtual void execute(const JsonDocument& doc) = 0;
+    virtual void update() {}
+
+protected:
+    bool isTargetPort(const JsonDocument& doc, const char* target) {
+        const char* port = doc["port"];
+        return port == nullptr || strcmp(port, target) == 0;
+    }
+};
+
+#endif // LIGHT_COMMAND_H
